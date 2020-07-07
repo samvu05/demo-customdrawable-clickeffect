ba644ae9138a04f42aacf39d1263d414376024b6
samvu.05 <samvu.it@gmail.com>
2020-07-07 14:38:50 +0700

    init project

---
+++ app/src/main/java/com/t3h/democustomdrawable/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.t3h.democustomdrawable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.demo_custom_drawable)
+    }
+}
